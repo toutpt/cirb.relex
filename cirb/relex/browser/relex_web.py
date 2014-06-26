bc5a9f2d1fabb6447e834b1ceefd369f75eba6f5
Gagaro <gagaro42@gmail.com>
2014-06-26 17:48:57 +0200

    fix: search in datatable

--- cirb/relex/browser/relex_web.py
+++ cirb/relex/browser/relex_web.py
@@ -1,5 +1,6 @@
 from Products.CMFPlone.utils import getToolByName
 from Products.Five.browser import BrowserView
+from zope.component import getMultiAdapter
 
 
 class TreeView(BrowserView):
@@ -22,8 +23,23 @@ class TreeView(BrowserView):
 
 class SearchView(BrowserView):
     def __call__(self):
-        self.catalog = getToolByName(self.context, 'portal_catalog')
+        self.update()
         return self.index()
+
+    def update(self):
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+        context = self.context.aq_inner
+        portal_state = getMultiAdapter((context, self.request),
+                                       name=u'plone_portal_state')
+        self.current_language = portal_state.language()
 
     def getProjects(self):
         return self.catalog(portal_type="Project")
+
+    def getName(self, project):
+        if self.current_language == 'fr':
+            return project.getName_fr()
+        if self.current_language == 'en':
+            return project.getName_en()
+        if self.current_language == 'nl':
+            return project.getName_nl()
