55017825826c458b9efe207d9fae7a8a7c915c2b
Gagaro <gagaro42@gmail.com>
2014-07-08 17:21:14 +0200

    i18n

---
+++ cirb/relex/rebuild_i18n.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+../../bin/i18ndude rebuild-pot --pot locales/cirb.relex.pot --create cirb.relex .
+
+../../bin/i18ndude sync --pot locales/cirb.relex.pot locales/fr/LC_MESSAGES/cirb.relex.po
+../../bin/i18ndude sync --pot locales/cirb.relex.pot locales/en/LC_MESSAGES/cirb.relex.po
