cd0dd9e2c8116253e3eec92581db835031af1204
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-07-17 10:11:04 +0200

    unplug authorization checks

--- cirb/relex/admin/static/js/built.js
+++ cirb/relex/admin/static/js/built.js
@@ -29,10 +29,12 @@ angular.module('relex.controllers').controller('AuthorizedController', [
 		//$scope.loggedin = $cookies.__ac !== undefined;
 		//idea: hit the addform
 		var BASE_URL = settingsService.BASE_URL;
-
+		$scope.loggedin = true;
+/*
 		var redirectedPattern = '/acl_users/credentials_cookie_auth/require_login?came_from=';
 		$http.get(BASE_URL + '/createObject?type_name=Project').then(
 			function(data){
+				debugger;
 				if (data.headers('tm-finalurl').indexOf(redirectedPattern) >- 1){
 					$scope.loggedin = false;
 					window.location.replace(BASE_URL + '/login?came_from=' + encodeURIComponent(window.location.href));
@@ -43,6 +45,7 @@ angular.module('relex.controllers').controller('AuthorizedController', [
 				$scope.loggedin = false;
 				window.location.replace(BASE_URL + '/login');
 			});
+*/
 	}
 ]);
 /*global angular:false */
