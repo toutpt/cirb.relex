32fc804704881d9dcd30c9232b5cf7b130712b17
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-08-21 16:08:23 +0200

    feature(admin): add  force-upper-case directive and use it on almost all input

--- cirb/relex/admin/static/js/built.js
+++ cirb/relex/admin/static/js/built.js
@@ -865,6 +865,30 @@ angular.module('relex.controllers').controller('VocabularyController',[
 /*global angular:false */
 /*jshint strict: false*/
 
+angular.module('relex.directives').directive('forceUpperCase',
+    ['$parse', function($parse){
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function(scope, element, attrs, modelCtrl){
+                var makeUpper = function(inputValue) {
+                    if (inputValue === undefined){
+                        return inputValue;
+                    }
+                    var upperd = inputValue.toUpperCase();
+                    if(upperd !== inputValue) {
+                        modelCtrl.$setViewValue(upperd);
+                        modelCtrl.$render();
+                    }
+                    return upperd;
+                };
+                modelCtrl.$parsers.push(makeUpper);
+                makeUpper($parse(attrs.ngModel)(scope));
+            }
+        };
+    }]
+);
+
 angular.module('relex.directives').directive('selectMultiple',
     ['$filter', 'langService', 'vocabularyService', function($filter, langService, vocabularyService){
         return {
@@ -879,14 +903,15 @@ angular.module('relex.directives').directive('selectMultiple',
                 filterMethod: '=',
                 filterParam: '='
             },
-            link: function(scope, elem, attrs){
+            link: function(scope){
                 scope.limitTo = 5;
                 scope.selected = true;
                 scope.showFilter = true;
                 scope.getLimitTo = function(){
-                    if (parseInt(scope.limitTo) <= 0)
+                    if (parseInt(scope.limitTo) <= 0){
                         return scope.vocabulary.length;
-                    return parseInt(scope.target.length) + parseInt(scope.limitTo);
+                    }
+                    return parseInt(scope.target.length, 10) + parseInt(scope.limitTo, 10);
                 };
                 scope.isSelected = function(term){
                     for (var i = 0; i < scope.target.length; i++) {
@@ -895,19 +920,17 @@ angular.module('relex.directives').directive('selectMultiple',
                                 term._selected = 1;
                                 return true;
                             }
-                        }else{
-                            console.error('null term in select-multiple');
-                            /*
-                    > term
-                    Object {code: Object, id: "59", description: Object}
-                    > scope.target
-                    [Object, null, null]
-                    > scope.target[0]
-                    Object {code: Object, id: "32", description: Object}
-                    > scope.target[0].code
-                    Object {fr: "CEX", en: "CEX", nl: "CEX"}
-                             */
                         }
+                        /*
+                        > term
+                        Object {code: Object, id: "59", description: Object}
+                        > scope.target
+                        [Object, null, null]
+                        > scope.target[0]
+                        Object {code: Object, id: "32", description: Object}
+                        > scope.target[0].code
+                        Object {fr: "CEX", en: "CEX", nl: "CEX"}
+                         */
                     }
                     term._selected = 0;
                     return false;
@@ -928,24 +951,24 @@ angular.module('relex.directives').directive('selectMultiple',
                     // Selected first
                     var selected = '1';
                     if (scope.isSelected(term))
-                        selected = '0';
+                        {selected = '0';}
                     if (!scope.selected)
-                        selected = '';
+                        {selected = '';}
                     // Country has a code attribute which is a String
                     if (term.hasOwnProperty('code') && term.code instanceof Object)
-                        return selected + scope._(term.code);
+                        {return selected + scope._(term.code);}
                     if (term.hasOwnProperty('name'))
-                        return selected + scope._(term.name)
+                        {return selected + scope._(term.name);}
                     if (term.hasOwnProperty('lastname'))
-                        return selected + term.lastname
+                        {return selected + term.lastname;}
                     if (term.hasOwnProperty('id'))
-                        return selected + scope._(term.id)
-                    return selected
+                        {return selected + scope._(term.id);}
+                    return selected;
                 };
                 scope.filter = function(term){
                     if (scope.queryTerm !== undefined && scope.queryTerm.length !== 0) {
                         if ($filter('filter')([term], scope.queryTerm).length === 0)
-                            return false;
+                            {return false;}
                     }
                     if (scope.showFilter && scope.filterMethod !== undefined) {
                         return scope.filterMethod(term, scope.filterParam);
