c5b017f5d154e61bb1e50b19cc79e55891cf3c01
Gagaro <gagaro42@gmail.com>
2014-10-09 16:58:27 +0200

    fix: workflow translations + french i18n for admin/front

--- cirb/relex/rebuild_i18n.sh
+++ cirb/relex/rebuild_i18n.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-../../bin/i18ndude rebuild-pot --pot locales/cirb.relex.pot --create cirb.relex .
+../../bin/i18ndude rebuild-pot --exclude 'node_modules bower_components' --pot locales/cirb.relex.pot --create cirb.relex .
 
 ../../bin/i18ndude sync --pot locales/cirb.relex.pot locales/fr/LC_MESSAGES/cirb.relex.po
 ../../bin/i18ndude sync --pot locales/cirb.relex.pot locales/en/LC_MESSAGES/cirb.relex.po
