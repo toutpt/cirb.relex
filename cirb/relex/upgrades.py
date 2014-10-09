0272baf535983a64f090c10d28267fb9fdac5a84
Gagaro <gagaro42@gmail.com>
2014-10-09 17:40:30 +0200

    fix: add upgrade step to convert bad reference in terms

--- cirb/relex/upgrades.py
+++ cirb/relex/upgrades.py
@@ -3,8 +3,10 @@ PROFILE = 'profile-cirb.relex:default'
 
 
 def fixOrganisationAndThemeRelation(context):
-	"""fix(data): organisation & theme relations array issues
-	['1', '2', ['3'], ['4']] ->['1', '2', '3', '4']"""
+        """
+        fix(data): organisation & theme relations array issues
+	['1', '2', ['3'], ['4']] ->['1', '2', '3', '4']
+        """
 	relex = context.relex_web
 	view = relex.restrictedTraverse('@@relex_vocabulary')
 	organisations = view._getTerms('organisation')
@@ -29,6 +31,24 @@ def fixOrganisationAndThemeRelation(context):
 				new_cells.append(cell)
 		organisation['cell'] = new_cells
 	view.dump('organisation', organisations)
+
+
+def fixIdInDict(context):
+    vocabularies_id = (
+            'country', 'region', 'city', 'organisation', 'contact',
+            'brusselspartners'
+    )
+    relex = context.relex_web
+    view = relex.restrictedTraverse('@@relex_vocabulary')
+    for vocabulary_id in vocabularies_id:
+        terms = view._getTerms(vocabulary_id)
+        for term in terms:
+            for key, value in term.items():
+                if type(value) is dict:
+                    if 'id' in value.keys():
+                        term[key] = value['id']
+        view.dump(vocabulary_id, terms)
+
 
 def common(context):
     setup = getToolByName(context, 'portal_setup')
